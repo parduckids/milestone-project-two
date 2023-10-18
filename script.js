9bb28df7f3b5b1d310c4f84b1f61c50cfe25c630
Adam V <parduckids@gmail.com>
2023-10-18 14:08:43 +0000

    Feat: add fetchDefinitions to retrieve and log API data based on input

--- script.js
+++ script.js
@@ -25,9 +25,51 @@ async function fetchSynonyms(word) {
         if (response.ok) {
             // Parse the response as JSON
             const result = await response.json();
-             // If the 'synonyms' property exists and has items, log each synonym to the console     
+             // If the 'synonyms' property exists and has items, log each synonym to the console    
+            console.log("SYNONYMS:") 
             for (let i = 0; i < result.synonyms.length; i++) {
                 console.log(result.synonyms[i]);
+            }
+        
+        } else {
+            console.log("No data found")
+        }
+    // Log any errors related to the fetch operation to the console    
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+/**
+ * Fetches definitions for a given word from the WordsAPI.
+ * 
+ * The function takes a word as input and constructs a URL to fetch definition for the word
+ * from the WordsAPI. If the fetch operation is successful and definitions are found, they are
+ * logged to the console. If no definitions are found or if there's an error with the fetch operation, 
+ * appropriate error messages are logged.
+ */
+async function fetchDefinitions(word) {
+    // Construct the URL using the provided word
+    const url = 'https://wordsapiv1.p.rapidapi.com/words/' + word + '/definitions';
+    // Set fetch options, including headers for authentication with the API
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'fe209f256emshc114ceae6c6c116p1d872djsn27d575353cc3',
+            'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+        }
+    };
+    try {
+         // Send a fetch request to the API
+        const response = await fetch(url, options);
+         // Check if the response was successful
+        if (response.ok) {
+            // Parse the response as JSON
+            const result = await response.json();
+             // If the 'definitions' property exists and has items, log each definition to the console  
+            console.log("DEFINITIONS:")    
+            for (let i = 0; i < result.definitions.length; i++) {
+                console.log(result.definitions[i].definition);
             }
         
         } else {
@@ -63,6 +105,7 @@ function fetchFromEntry(event) {
     if (inputValue != '') {
         // If the input is not empty, use the fetchSynonyms function to fetch data from the api using the input value
         fetchSynonyms(inputValue);
+        fetchDefinitions(inputValue);
 
         // Present the input value on the UI
         choosenWord.html('Your word is: <b>' + inputValue + '</b>');
